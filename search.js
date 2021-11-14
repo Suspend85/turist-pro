0174cfe3eba85976e77b207b422f3aaee0e34b5a
w03 <w03@dom201.local>
2021-11-14 14:41:55 +0700

    First commit

---
+++ search.js
@@ -0,0 +1,55 @@
+const tbody = document.getElementById("search-res-tbody");
+
+const trips = [
+  {
+    id: 0,
+    region: "Москва",
+    name: "Сноуборд",
+    level: "Легкий",
+    start: "09-03-22",
+    end: "14-03-22",
+    zones: 5,
+  },
+  {
+    id: 2,
+    region: "Санкт-Петербург",
+    name: "Лыжи",
+    level: "Легкий",
+    start: "15-03-22",
+    end: "20-03-22",
+    zones: 5,
+  },
+];
+
+const renderRow = ({ id, region, name, level, start, end, zones }) =>
+  `<tr>
+    <td>${region}</td>
+    <td>${name}</td>
+    <td>${level}</td>
+    <td>${start}</td>
+    <td>${end}</td>
+    <td>${zones}</td>
+    <td><button type="button" data-id="${id}">Выбрать</button></td>
+</tr>`;
+
+const urlSearchParams = new URLSearchParams(window.location.search);
+const params = Object.fromEntries(urlSearchParams.entries());
+const { region } = params;  // const region = params.region - тоже самое
+tbody.innerHTML = trips.filter(trip => trip.region === region).map(renderRow).join("");
+
+// console.log(region);
+
+tbody.addEventListener('click', event => {
+    if (event.target.nodeName !== "BUTTON") {
+        return
+    }
+    const id = +event.target.dataset.id
+    const trip = trips.find(trip => trip.id === id)
+    window.localStorage.setItem('chosenTrip', JSON.stringify(trip))
+    window.location.href = "seat.html"   // window.open('seat.html') - открыть в новом окне
+
+    // console.log(id);
+    // console.log(trip);
+    // console.log(event.target.type);
+    // console.log(event.target.nodeName)
+})
